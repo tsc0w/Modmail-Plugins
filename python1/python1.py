2d59b506b5d08f63848f6aa1fde2a8039ef81bbb
tsc0w <67469685+tsc0w@users.noreply.github.com>
2020-06-26 16:31:25 +0100

    Update python1.py

--- python1/python1.py
+++ python1/python1.py
@@ -13,11 +13,11 @@ class MyCog(commands.Cog):
     async def echo(self, ctx, *, message):
         await ctx.send(message)
 
-    @commands.command()
-    async def suggest(self, ctx):
-    e = discord.Embed(title="Suggestions for a Flickz Bot Command?", description="If you have a suggestion, please DM me it and i will be able to check it asap! ", color=0xf542f2)
-    e.add_field(name="*Flickz V1.85*", value="**DM ME FOR ANY COMMAND SUGGESTIONS!**")
-    await ctx.send(embed=e)
+    @client.event
+    async def on_message(message):
+        if message.content == 'ft/pingo':
+            await message.channel.send("pong")
+        await client.process_commands(message)
         
 def setup(bot):
     bot.add_cog(MyCog(bot))
