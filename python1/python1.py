233cf0b44fbe4dbe36366fe4afd391ae811fa1dd
tsc0w <67469685+tsc0w@users.noreply.github.com>
2020-06-26 15:27:31 +0100

    python1.py
    descriptoiosj

---
+++ python1/python1.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+from discord.ext import menus
+import random
+from discord import Game
